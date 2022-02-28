f767f7570dd94a2a6e3623d454817ea1e1add966
jimmihu <jimmihuszz@gmail.com>
2022-02-28 17:04:01 +0700

    try2

--- handlers/handlerUser.go
+++ handlers/handlerUser.go
@@ -14,7 +14,7 @@ import (
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	Checkcookies(w, r)
 	Refreshcookies(w, r)
-	fmt.Println("Welcome! by Jimmi")
+	fmt.Fprintf(w, "Welcome! by Jimmi")
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
