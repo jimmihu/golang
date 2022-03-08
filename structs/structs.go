525eb831bcdff657edb9beea5c20ac8eaf0a3709
jimmihu <jimmihuszz@gmail.com>
2022-03-08 23:51:32 +0700

    test

--- structs/structs.go
+++ structs/structs.go
@@ -2,7 +2,7 @@ package structs
 
 //struct risk_profile dgn foreign key ke user ID
 type Risk_profile struct {
-	UserID        int     `json:"userid"`
+	UserID        int     `json:"userid" gorm:"primaryKey"`
 	MM_percent    float32 `json:"mm_percent"`
 	Bond_percent  float32 `json:"bond_percent"`
 	Stock_percent float32 `json:"stock_percent"`
@@ -10,11 +10,11 @@ type Risk_profile struct {
 
 //struct user
 type User struct {
-	ID           int          `json:"id"`
+	ID           int          `json:"id" gorm:"primaryKey"`
 	Name         string       `json:"name"`
 	Age          int          `json:"age"`
 	Password     string       `json:"password"`
-	Risk_profile Risk_profile `json:"risk_profile" gorm:"primaryKey"`
+	Risk_profile Risk_profile `json:"risk_profile" `
 }
 
 // Result is an array of post
